fbaa7512a65ae457f6dcbd6e27791d2fba289ea8
MBartscher <43210537+MBartscher@users.noreply.github.com>
2023-05-24 10:12:28 +0200

    work status before presentation

--- Martin/gui.py
+++ Martin/gui.py
@@ -1,9 +1,14 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTabWidget, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QFormLayout, QSpacerItem, QPushButton, QSizePolicy
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTabWidget, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QFormLayout, QSpacerItem, QPushButton
 from PyQt6.QtCore import Qt
 
 username = "test"
 
-# class welcome_label?! -> creating object of it in tabs!
+
+class Welcome_Label(QLabel):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setText(f"Hello {username}")
+        self.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
 
 class Home(QMainWindow):
@@ -14,15 +19,18 @@ class Home(QMainWindow):
         # home tab
         self.home_tab = QWidget()
         self.home_tab_layout = QVBoxLayout()
-        self.welcoming_at_user = QLabel(text=f"Hello {username}")
-        self.welcoming_at_user.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self.home_tab_layout.addWidget(self.welcoming_at_user)
+        # home welcome label
+        self.home_welcoming = Welcome_Label()
+        self.home_tab_layout.addWidget(self.home_welcoming)
+        # home output trainings plan
         self.out_train_plan = QWidget()
         # out_train_plan needs to be filled with the relevant elements...
         self.home_tab_layout.addWidget(self.out_train_plan)
+        # home buttons
         self.home_btns = QWidget()
         self.home_btns_layout = QHBoxLayout()
         self.home_btns_layout.addItem(QSpacerItem(0, 0))
+        # button generate trainings plan
         self.home_btn_gen_plan = QPushButton("generate trainings plan")
         self.home_btns_layout.addWidget(self.home_btn_gen_plan)
         self.home_btns.setLayout(self.home_btns_layout)
@@ -32,39 +40,74 @@ class Home(QMainWindow):
         # session tab
         self.session_tab = QWidget()
         self.session_tab_layout = QVBoxLayout()
-        self.welcoming_at_session = QLabel(text=f"Hello {username}")
-        self.welcoming_at_session.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.session_tab_layout.addWidget(self.welcoming_at_session)
+        # session welcome label
+        self.session_welcoming = Welcome_Label()
+        self.session_tab_layout.addWidget(self.session_welcoming)
+        # session output last session
         self.out_last_session = QWidget()
         # out_last_session needs to be filled with the relevant elements...
         self.session_tab_layout.addWidget(self.out_last_session)
+        # session buttons
         self.session_btns = QWidget()
         self.session_btns_layout = QHBoxLayout()
         self.session_btns_layout.addItem(QSpacerItem(0, 0))
+        # button change last session
         self.session_btn_change = QPushButton("change last session")
         self.session_btns_layout.addWidget(self.session_btn_change)
+        # button insert new session
         self.session_btn_insert = QPushButton("insert new session")
         self.session_btns_layout.addWidget(self.session_btn_insert)
         self.session_btns.setLayout(self.session_btns_layout)
         self.session_tab_layout.addWidget(self.session_btns)
         self.session_tab.setLayout(self.session_tab_layout)
         self.tab_widget.addTab(self.session_tab, "session")
+        # params tab
+        self.params_tab = QWidget()
+        self.params_tab_layout = QVBoxLayout()
+        # params welcome label
+        self.params_welcoming = Welcome_Label()
+        self.params_tab_layout.addWidget(self.params_welcoming)
+        # params 2 columns
+        self.params = QWidget()
+        self.params_layout = QHBoxLayout()
+        # params column body
+        self.params_body = QWidget()
+        self.params_body_layout = QVBoxLayout()
+        self.out_body_params = QWidget()
+        # out_body_params needs to be filled with the relevant elements...
+        self.params_body_layout.addWidget(self.out_body_params)
+        self.params_insert_body_params = QPushButton("insert body params")
+        self.params_body_layout.addWidget(self.params_insert_body_params)
+        self.params_body.setLayout(self.params_body_layout)
+        self.params_layout.addWidget(self.params_body)
+        self.params_goals = QWidget()
+        self.params_goals_layout = QVBoxLayout()
+        # missing contents params goals
+        self.params_goals.setLayout(self.params_goals_layout)
+        self.params_layout.addWidget(self.params_goals)
+        self.params_tab_layout.addWidget(self.params)
+        self.params_tab.setLayout(self.params_tab_layout)
+        self.tab_widget.addTab(self.params_tab, "params")
         # other tabs
+        ###
+        # tab widget to central widget
         self.setCentralWidget(self.tab_widget)
+        # add actions to widgets
         self.home_btn_gen_plan.clicked.connect(self.generate_trainings_plan)
         self.session_btn_change.clicked.connect(self.change_last_session)
         self.session_btn_insert.clicked.connect(self.insert_session)
+        self.params_insert_body_params.clicked.connect(self.insert_body_params)
 
     def generate_trainings_plan(self):
-        # action generate trainingsplan
         pass
 
     def change_last_session(self):
-        # action change last session
         pass
 
     def insert_session(self):
-        # action insert session
+        pass
+
+    def insert_body_params(self):
         pass
 
 
